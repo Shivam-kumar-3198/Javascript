8623391aae444111a59809094e836ce585a6092d
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-05 23:15:25 +0530

    Variablesfullclear.js

---
+++ Variablesfullclear.js
@@ -0,0 +1,25 @@
+// console.log("This is shivam")
+const accountId = 11222809
+let accountEmail = "thisisshivamkumar8840gmail.com"
+var accountPassword = "12345"
+accountCity = "Mullana"
+let accountstate;
+// const,let, var these are use to store memory
+
+// accountId = 2
+accountEmail = "hsbc3252@gmail.com"
+accountCity  = "Bengaluru"
+accountPassword = "54321"
+
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountCity, accountPassword, accountstate]);
+// prefer not use var bacause of issue in block scope and functional scope
+
+const meHere = 11222809
+let details = "My name is Shivam"
+var matters = "I am Incredible"
+
+console.log(meHere);
+console.log(details);
+console.log(matters);
+console.log(accountCity);
