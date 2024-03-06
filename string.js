b2ebdb80b5512827a56c7d2a863288e05f2c5e9b
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-06 19:16:19 +0530

    d:\Javascript\Javascript\string.js

---
+++ string.js
@@ -0,0 +1,7 @@
+const name = "hitesh" //String is declared in this way
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String ('hitesh')
