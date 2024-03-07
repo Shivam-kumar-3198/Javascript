04cbe4947cc089ec43c7039376ecaee75bc15715
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-07 23:00:00 +0530

    d:\Javascript\Javascript\Operations.js

---
+++ Operations.js
@@ -0,0 +1,19 @@
+let value = 3;
+let noneg = -value;
+console.log("Your value is turned negative");
+console.log(noneg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**26);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Shivam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
