e06749f37d2234edd5ec8eb30a25230e7733aa6e
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-08 19:12:05 +0530

    d:\Javascript\Javascript\numberconversion.js

---
+++ numberconversion.js
@@ -0,0 +1,28 @@
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("a" +"3")
+console.log(1+2+"2");
+
+console.log((3+4) * 5 % 4);
+
+console.log(+true);
+// console.log(true+);This is not accepted by compiler
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 200
+++gamecounter;
+console.log(gamecounter);
