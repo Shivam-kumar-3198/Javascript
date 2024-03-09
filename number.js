2a51de7f228448a5a18c1049145c0fbbd56b1140
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-09 09:22:07 +0530

    d:\Javascript\Javascript\number.js

---
+++ number.js
@@ -0,0 +1,35 @@
+// // const score = 400;
+// //         const balance = new Number(100);
+// //         console.log(balance);
+// //         console.log(score);
+
+//         console.log(balance.toString().length);
+//         console.log(balance.toFixed(7));
+
+//         const nextNumber = 23.896;
+//         // const nextNumber = 123.896;
+//         console.log(nextNumber.toPrecision(2));
+
+//         const hundreds = 1000000;
+//         console.log(hundreds.toLocaleString());
+
+        //////////////// maths ///////////////////
+        console.log(Math);
+        console.log(Math.abs(-4));//absolute makes negative to postive
+        console.log(Math.round(4.6));//ye Round off values dega
+        console.log(Math.ceil(4.2));//ye uper value show krega
+        console.log(Math.floor(4.9));//ye lower value show krega
+        console.log(Math.min(4,3,2,9)); // Ye minimum value ko btayega
+        console.log(Math.max(10,12,13,14));//ye maximum values ko show krega
+        console.log(Math.log10(2));
+
+        /////////////////Where to use ////////////////////////
+console.log(Math.random());//math.random humesa values aayenge 0 se 1 ke bich me
+// console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random())*10);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random()* (max-min+1)))
+
