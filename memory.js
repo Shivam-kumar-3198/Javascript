07edb7b2c7ab511cadd4339314452460303d8755
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-09 15:56:15 +0530

    d:\Javascript\Javascript\memory.js

---
+++ memory.js
@@ -0,0 +1,12 @@
+// stack (primitive)--->stack memory is used in primitve , In this you recieve the copy ofa variable ,
+//  Heap (Non primitve)--->reference of original value every change will be in original value
+
+let myYoutubename = "the path"
+
+let anothername = "mychannelname"
+anothername = "techy"
+
+console.log(anothername);
+console.log(anothername);
+
+// stack and heap
