6c20effcd6e81b2504eb260157372c58c1920984
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-11 18:25:55 +0530

    d:\Javascript\Javascript\13.)Date&time.js

---
+++ 13.)Date&time.js
@@ -0,0 +1,26 @@
+//dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+
+
+// let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp = Date.now()// milliseconds me values dega 1-14-2023 ka
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now()/1000);// to remove decimal in output math.floor ka use krenge
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDay());
+
+//`${newDate.getDay()} and the time`
