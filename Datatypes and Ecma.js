873f0831cd8fc088b9d67601e887601a266784b7
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-13 18:28:54 +0530

    d:\Javascript\Javascript\Datatypes and Ecma.js

---
+++ Datatypes and Ecma.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3
+    +
+    3); // Focus on maintaining code documentation or readability
+
+console.log("Hey");
+
+let name = "Me"
+let age = 18
+let isLoggedin = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+console.log(typeof "name");
+// string
+console.log(typeof age);
+// number
+console.log(typeof null); 
+// object
+console.log(typeof isLoggedin);
