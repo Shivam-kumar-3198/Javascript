34ebd60d92a41e9e2e5875766298273fdaf61cd3
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-16 11:48:00 +0530

    d:\Javascript\Javascript\16.)object.js

---
+++ 16.)object.js
@@ -0,0 +1,35 @@
+//singleton
+
+// object literals
+//code here
+// const mySym = Symbol("Key1")
+
+// const DjUser ={ 
+//     name: "Manish" ,
+//     "fullname" : "Manisha Rani",
+// }
+// console.log(DjUser.name);
+// console.log(DjUser.fullname);
+// const JsUser = {
+//     name: "Shivam",
+//     "fullname" : "Shivam Kumar",
+//     [mySym] : "Key1",
+//     age :18,
+//     location : "ambala",
+//     isLoggedIn : false,
+//     lastLoginDays : ["Monday", "Saturday"]
+// } 
+// console.log(JsUser.name);
+// console.log(JsUser.age);
+// console.log(JsUser.location);
+// console.log(JsUser.isLoggedIn);
+// console.log(JsUser.lastLoginDays);
+//code ends
+
+// another way of using object value if there is string
+
+console.log(JsUser["fullname"]); //easy to access value
+console.log(mySym);
+
+
+ 
