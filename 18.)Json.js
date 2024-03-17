959aa7ff54fb60a4fa324f9ac7eac40dee6208a7
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-17 18:46:03 +0530

    d:\Javascript\Javascript\18.)Json.js

---
+++ 18.)Json.js
@@ -0,0 +1,19 @@
+// const course = {
+//     coursename: "This is Shivam",
+//     price: "999",
+//     courseInstrutor: "Shivam",
+// }
+
+// //course.courseInstrutor
+// const {courseInstrutor} = course
+// console.log(courseInstrutor);
+
+//de Structure of objectx
+/*
+const navbar = ({company}) => {
+
+}
+navbar(company = "Shivam")
+*/
+
+//values in Api
