b3a16fed99d9cdb74e38a3fda1f9b4e3e0dba775
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-17 18:41:45 +0530

    d:\Javascript\Javascript\17.)object2.js

---
+++ 17.)object2.js
@@ -0,0 +1,34 @@
+// const course = {
+//     coursename: "Js in hindi",
+//     price: 99,
+//     courseInstructor: "Shivam"
+// }
+
+// //course.courseInstructor It is only syntax
+
+// const {courseInstructor} = course
+
+// console.log(courseInstructor);
+
+// next code
+
+//const tinderUser = new Object()
+// learning object
+// const meShivam = {}
+// meShivam.Roll = "11222809"
+// meShivam.name = "Shivam kumar"
+// meShivam.isLoggedIn = false
+
+// console.log(meShivam);
+
+// object ke andar object
+const mySpace ={
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname :{
+            firstname:"Shivam",
+            lastname: "singh"
+        }
+    }
+} 
+console.log(userfullfullname.name.firstname);
