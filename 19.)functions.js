c830b3370f09d66409d3da090c69eb04c2677060
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-18 00:29:46 +0530

    d:\Javascript\Javascript\19.)functions.js

---
+++ 19.)functions.js
@@ -0,0 +1,43 @@
+//FUNCTION CALLING
+/*
+function myNameShivam(){
+    console.log("S");
+    console.log("h");
+    console.log("i");
+    console.log("v");
+    console.log("a");
+    console.log("m");
+}
+*/
+/*
+myNameShivam();
+
+function addTwoNumbers(number1, number2){
+       console.log(number1 + number2);
+}
+
+addTwoNumbers(3,10)
+*/
+/*
+function learnMultiplication(number1 ,number2) {
+    console.log(number1*number2);
+    
+}
+
+learnMultiplication(10,90)
+*/
+/*
+function add(number1,number2){
+    console.log(number1+number2);
+}
+
+add(34,"a")
+*/
+/*
+//By using Return and string Interpolation
+function loginUserMessage(username){
+    return `${username} Just logged in` //Be careful `${message}`
+}
+console.log(loginUserMessage("Shivam")) 
+*/
+
