9f8bf2c24aaf604457746daa976e0d0680920d1e
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-19 17:13:32 +0530

    d:\Javascript\Javascript\22.)Hoisting.js

---
+++ 22.)Hoisting.js
@@ -0,0 +1,37 @@
+/*
+function one(){
+    const username = "shivam"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+*/
+/* If condition
+if (true) {
+    const username = "Shivam"
+    if (username==="Shivam") 
+    {
+       const website = "Youtube"
+       console.log(username + website);    
+    }
+    // console.log(website);
+}
+// console.log(username);
+*/
+// +++++++++++++ Interesting +++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+const addTwo = function(num) {
+    return num + 2
+    
+}
+addTwo(5)
