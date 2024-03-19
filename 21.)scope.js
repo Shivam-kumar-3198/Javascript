28554855c0b6fd2ebef8b51311b9c123cb5fb054
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-19 17:12:17 +0530

    d:\Javascript\Javascript\21.)scope.js

---
+++ 21.)scope.js
@@ -0,0 +1,24 @@
+/*
+// let a = 30;
+// const b = 40;
+// var c = 50;
+
+if (true) {
+    let a = 70;
+    const b = 80;
+    var c = 90;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+output = 90
+*/
+let a = 300;
+if (true) {
+    let a = 10
+    const b =20
+    console.log("sorry bro",a);
+}
+console.log(a);
