ed516e18f5889d9f920fd03b084fefcbd2b2ef59
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-22 18:28:12 +0530

    d:\Javascript\Javascript\26.3)switch.js

---
+++ 26.3)switch.js
@@ -0,0 +1,53 @@
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+/*
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default class");
+        break;
+}
+*/
+// it can also store string
+const month = "April"
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "february":
+        console.log("february");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default class");
+        break;
+}
