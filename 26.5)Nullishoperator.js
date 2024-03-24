8b9882cd282e93d5f1464c1859b4a3f71c519309
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-24 08:30:59 +0530

    d:\Javascript\Javascript\26.5)Nullishoperator.js

---
+++ 26.5)Nullishoperator.js
@@ -0,0 +1,11 @@
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1);
+
+//terniary operartor
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
