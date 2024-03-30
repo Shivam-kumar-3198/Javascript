99d37b6cf68d94b516e9a566e87d771fb3c92530
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-31 00:19:59 +0530

    d:\Javascript\Javascript\27.4)breakandconti.js

---
+++ 27.4)breakandconti.js
@@ -0,0 +1,21 @@
+/*
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log('Detected 5');
+        break
+    }
+    console.log(`value of i is ${i}`);
+    
+}
+*/
+
+//continue
+
+for (let i = 0; i <=20; i++) {
+    if (i == 5) {
+        console.log('detcted 5');
+        continue
+    }
+    console.log(`value of i is ${i}`);
+    
+}
