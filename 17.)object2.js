bc35cfa02d1a37ae161c42190bb63a756fc378d6
Shivam <thisisshivamkumar8840@gmail.com>
2024-04-18 18:58:28 +0530

    d:\Javascript\Javascript\17.)object2.js

--- 17.)object2.js
+++ 17.)object2.js
@@ -22,6 +22,7 @@
 // console.log(meShivam);
 
 // object ke andar object
+/*
 const mySpace ={
     email: "abc@gmail.com",
     fullname: {
@@ -32,3 +33,61 @@ const mySpace ={
     }
 } 
 console.log(userfullfullname.name.firstname);
+*/
+
+
+//singleton
+
+// object literals
+const mySym = Symbol("key1")
+
+const Shivam = {
+    name: "Don",
+    age: "18",
+    [mySym]: "key1",
+    location: "jaipur",
+    email: "123@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays:["Monday", "Saturday"],
+
+    "full name" : "Shivam Kumar Singh"
+}
+
+//then how to access above objects
+
+//this is another way of accessing  console.log(Shivam.lastLoginDays)
+
+/*
+console.log(Shivam["isLoggedIn"])
+console.log(Shivam["full name"]);
+console.log(Shivam[mySym]);
+
+Shivam.email = "hayabusa@123"
+Object.freeze(Shivam) //Add values
+Shivam.email = "dukati@321"
+console.log(Shivam);
+*/
+//output
+/*
+{
+  name: 'Don',
+  age: '18',
+  location: 'jaipur',
+  email: 'hayabusa@123',
+  isLoggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  'full name': 'Shivam Kumar Singh',
+  [Symbol(key1)]: 'key1'
+}
+*/
+
+Shivam.greeting = function(){
+    console.log("Hello Shivam");
+}
+
+Shivam.greetingTwo = function(){
+    console.log(`Hello Shivam, ${this.name}`); //String Iinterpolation
+}
+
+console.log(Shivam.greeting());
+console.log(Shivam.greetingTwo());
