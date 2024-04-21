4e66ea4aa20ddf894d0a18b7655d1afd79bafa9e
Shivam <thisisshivamkumar8840@gmail.com>
2024-04-21 08:07:55 +0530

    d:\Javascript\Javascript\29.2)Maps.js

---
+++ 29.2)Maps.js
@@ -0,0 +1,21 @@
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of Ameroca")
+map.set('Fr', "France")
+map.set('BN', "Bangladesh")
+map.set('Indo', "Indonesia")
+
+//console.log(map);
+
+for (const [hi,data] of map) {
+   // console.log(hi, ':-', data);
+}
+
+const myObject= {
+    'game1' : 'NFS'
+    
+    
+}
+
+console.log(myObject);
