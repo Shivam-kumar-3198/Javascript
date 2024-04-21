7afb38454bb416549b668d773beaa00093a3ab07
Shivam <thisisshivamkumar8840@gmail.com>
2024-04-21 12:13:24 +0530

    d:\Javascript\Javascript\29.6)foreach2.js

---
+++ 29.6)foreach2.js
@@ -0,0 +1,20 @@
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName:"js"
+    },
+
+    {
+        languageName: "C++",
+        languageFileName:".cpp"
+    },
+
+    {
+        languageName: "Python",
+        languageFileName:"py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageFileName);
+})
