a5ef39fe6cfe46f2cb2289c35bc2b52eeb6a19b3
Shivam <thisisshivamkumar8840@gmail.com>
2024-04-23 23:41:02 +0530

    d:\Javascript\Javascript\29.1)arrayloopsthree.js

--- 29.1)arrayloopsthree.js
+++ 29.1)arrayloopsthree.js
@@ -8,7 +8,7 @@
 const arr = [1,2,3,4,5]
 
 for (const shivam of arr) { //here object refers to array
-//  console.log(shivam);   
+   // console.log(shivam);   
 }
 
 const greetings = "hello user"
@@ -24,5 +24,11 @@ for (const hi of Shivam) {
     console.log(`Welcome to the world ${Shivam}`);
 }
 
+
+const robinhood = "Achha Chor"
+
+for (const daring of robinhood) {
+    console.log(`England ke Itihas ka Sabse ${robinhood} hai robinhood`);
+}
 
 
