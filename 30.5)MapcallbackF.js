822a34acb7325b89b04763f59b53ffc8e41993f5
Shivam <thisisshivamkumar8840@gmail.com>
2024-04-28 22:28:50 +0530

    d:\Javascript\Javascript\30.5)MapcallbackF.js

---
+++ 30.5)MapcallbackF.js
@@ -0,0 +1,12 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const newNumbers = myNumbers.map( (num) => num + 10)
+
+//console.log(newNumbers);
+
+
+const chunaoDates = [13,26,14,16,17,18,19]
+
+const newChunaoDates = chunaoDates.map ((num) => (num + 5))
+
+console.log(`Here are your New Chunao Dates ${newChunaoDates}`);
