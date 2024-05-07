612caf1eb93fe2d9cb8f676cca806d86ccf9ddea
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-07 08:16:51 +0530

    d:\Javascript\Javascript\35.)Project Folder\readme.md

---
+++ 35.)Project Folder/script.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+// console.log(buttons);
+
+const body = document.querySelector('body');
+// console.log(body);
+
+// You can use forEach with NodeLists.
+buttons.forEach(function (button) {
+    // console.log(button);
+    button.addEventListener('click', function (hi) {
+       // console.log(e.target);
+       // console.log(e);
+       if(hi.target.id === 'grey'){
+        body.style.backgroundColor = "grey";
+       }
+
+       if (hi.target.id === 'white') {
+        body.style.backgroundColor = "whitesmoke" 
+       }
+
+       if(hi.target.id === 'blue'){
+        body.style.backgroundColor = "blue"
+       }
+
+       if(hi.target.id === 'yellow' ){
+        body.style.backgroundColor = 'yellow'
+       }
+    });
+});
