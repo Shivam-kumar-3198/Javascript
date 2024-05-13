a3ea2f67aa9a0dfdfd72407a7b7228a61deb182e
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-13 09:55:55 +0530

    d:\Javascript\Javascript\35.2)Digital clock

---
+++ 35.2)Digital clock/script.js
@@ -0,0 +1,14 @@
+const clock = document.getElementById('clock')
+//document.querySelector('#clock')
+
+let date = new Date ()
+console.log(date.toLocaleTimeString());
+
+
+//setInterval(function(){},2000)
+
+setInterval(function(){
+     let date = new Date();
+     clock.innerHTML = date.toLocaleTimeString();
+     
+},10000)
