cf6b6550cb13282b668cf41368801a860f6549b4
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-19 09:28:13 +0530

    d:\Javascript\Javascript\35.3)big project

--- 35.3)big project/script.js
+++ 35.3)big project/script.js
@@ -1,1 +1,36 @@
 console.log(Math.random()*1000);
+
+const submit = document.querySelector('#subt');
+const userInput =document.querySelector('#guessField');
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = ddocument.querySelector('.lowOrHi');
+const startOver = ddocument.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+// everything assigned succesfully we have to assign function
+
+function validateGuess(guess){
+//
+}
+function checkGuess(guess){
+//
+}
+
+function displayGuess(guess){
+    //
+}
+
+function displayMessage(message){
+    //
+}
+
+
+
+
