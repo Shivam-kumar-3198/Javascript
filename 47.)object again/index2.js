66e414184a84ea09f0058390e72d21ea1c44ed66
Shivam <thisisshivamkumar8840@gmail.com>
2024-06-23 18:48:40 +0530

    d:\Javascript\Javascript\47.)object again\index2.js

--- 47.)object again/index2.js
+++ 47.)object again/index2.js
@@ -1,20 +1,18 @@
-//We are Leraning descripter property
-
-const shivam ={
-    name:'das',
-    price:'20 Rupees',
+const shivam = {
+    name: 'das',
+    price: '20 Rupees',
     isAvailable: true
-}
+};
 
 console.log(Object.getOwnPropertyDescriptor(shivam, "name"));
 
 /* Output
 { value: 'das', writable: true, enumerable: true, configurable: true }
- */
+*/
 
-Oject.defineProperty(shivam, 'name', {
+Object.defineProperty(shivam, 'name', {
     writable: false,
-    enumerable:false
-})
+    enumerable: false
+});
 
 console.log(Object.getOwnPropertyDescriptor(shivam, "name"));
